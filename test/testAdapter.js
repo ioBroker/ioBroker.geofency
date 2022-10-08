cb1477f116ed9860da578cedaa38bdaaa2050ff2
Ingo Fischer <github@fischer-ka.de>
2022-10-08 12:40:58 +0200

    * optimize tests

--- test/testAdapter.js
+++ test/testAdapter.js
@@ -134,7 +134,7 @@ describe('Test ' + adapterShortName + ' adapter', function() {
 
         setup.stopController(function (normalTerminated) {
             console.log('Adapter normal terminated: ' + normalTerminated);
-            done();
+            setTimeout(done, 2000);
         });
     });
 });
